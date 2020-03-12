02bf88907d34a60c5f7f13efc3f09c8d2f7458b7
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2020-03-12 16:14:43 -0400

    Initial concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*!
+ * Copyright (C) 2020  Zachary Kohnen (DusterTheFirst)
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from "react";
+import { GlobalStyle } from "./styles";
+import useSpeech from "./useSpeech";
+
+/** The main app component */
+export default function App() {
+    const [sentence, setSentence] = useState("");
+    useSpeech(setSentence);
+
+    return (
+        <div>
+            <GlobalStyle />
+            <pre>{sentence}</pre>
+            <a href="http://socialcredit.gov/">socialcredit.gov</a>
+            <br/>
+            <a href="http://gogle.com/">gogle.com</a>
+            <br/>
+            <a href="http://shoppingsite.bruh/">shoppingsite.bruh</a>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/useSpeech.ts
@@ -0,0 +1,50 @@
+/*!
+ * Copyright (C) 2020  Zachary Kohnen (DusterTheFirst)
+ */
+
+import { useEffect } from "react";
+
+/* tslint:disable no-unsafe-any no-any */
+
+/** The speech recognition API */
+const SpeechRecognition: typeof window.SpeechRecognition = window.SpeechRecognition ?? (window as any).webkitSpeechRecognition;
+/** The speech grammar list API */
+// const SpeechGrammarList: typeof window.SpeechGrammarList = window.SpeechGrammarList ?? (window as any).webkitSpeechGrammarList;
+/** The speech recognition event API */
+// const SpeechRecognitionEvent: typeof window.SpeechRecognitionEvent = window.SpeechRecognitionEvent ?? (window as any).webkitSpeechRecognitionEvent;
+
+/* tslint:enable no-unsafe-any no-any */
+
+/** A hook to use the SpeechRecognition api */
+export default function useSpeech(callback: (sentence: string) => void) {
+    useEffect(() => {
+        let cont = true;
+
+        const recog = new SpeechRecognition();
+        recog.continuous = false;
+        recog.lang = "en-US";
+        recog.maxAlternatives = 1;
+
+        recog.start();
+
+        recog.onresult = (event) => {
+            console.log(event.results[0][0].transcript);
+            if (event.results[0].isFinal) {
+                callback(event.results[0][0].transcript);
+            }
+        };
+
+        recog.onend = () => {
+            if (cont) {
+                recog.start();
+            }
+        };
+
+        console.log(recog);
+
+        return () => {
+            cont = false;
+            recog.stop();
+        };
+    }, [callback]);
+}
